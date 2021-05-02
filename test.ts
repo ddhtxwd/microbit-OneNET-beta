1a4bbfa2b878a1ebb43b55aa62a0a61f43d2ab30
ddhtxwd <ddh895647891>
2021-05-02 16:07:00 +0800

    addir

--- test.ts
+++ test.ts
@@ -1,10 +1,5 @@
 // tests go here; this will not be compiled when this package is used as a library
-OneNET.WIFI_init(SerialPin.USB_TX, SerialPin.USB_RX)
-OneNET.WIFI_connect("XMU-STEAM", "mazhuankuxmu")
-OneNET.on_wifi_connected(function () {
-    basic.showNumber(1)
-    OneNET.OneNET_connect("279878", "550815340", "1234")
-})
-OneNET.on_mqtt_connected(function () {
-    basic.showNumber(1)
-})
+OneNET.WIFI_connect("WIFI", "12345678")
+OneNET.lcd_clear()
+OneNET.lcd_display_string(0, 0, "hello world")
+OneNET.lcd_display_number(0, 1, 666)
